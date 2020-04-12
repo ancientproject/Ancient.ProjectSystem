d60141047faffa80431aed1aa157ebf0bc8a64b2
Yuuki Wesp <ls-micro@ya.ru>
2020-04-12 07:55:20 +0300

    add rune spec and package classes

---
+++ csharp/Ancient.ProjectSystem/RunePackage.cs
@@ -0,0 +1,52 @@
+﻿namespace Ancient.ProjectSystem
+{
+    using System;
+    using System.IO;
+    using System.IO.Compression;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using exceptions;
+    using Newtonsoft.Json;
+    using NuGet.Versioning;
+
+    public class RunePackage : IDisposable, IAsyncDisposable
+    {
+        public string ID { get; set; }
+        public NuGetVersion Version { get; set; }
+        public MemoryStream Content { get; set; }
+        public RuneSpec Spec { get; set; }
+
+        public void Dispose()
+            => Content?.Dispose();
+        public ValueTask DisposeAsync()
+            => Content?.DisposeAsync() ?? new ValueTask(Task.CompletedTask);
+
+
+        public static async Task<RunePackage> Unwrap(Stream stream, CancellationToken cancellationToken = default)
+        {
+            using var zip = new ZipArchive(stream);
+            var entity = zip.GetEntry("target.rspec");
+
+            if (entity is null)
+                throw new CorruptedPackageException($"Could not find target.rspec in package.");
+
+            await using var point = entity.Open();
+            await using var memory = new MemoryStream();
+            await point.CopyToAsync(memory, cancellationToken);
+            var result = Encoding.UTF8.GetString(memory.ToArray());
+
+            var spec = JsonConvert.DeserializeObject<RuneSpec>(result);
+            var content = new MemoryStream();
+            await stream.CopyToAsync(content, cancellationToken);
+
+            return new RunePackage
+            {
+                Content = content,
+                ID = spec.ID,
+                Version = spec.Version,
+                Spec = spec
+            };
+        }
+    }
+}
---
+++ csharp/Ancient.ProjectSystem/RuneSpec.cs
@@ -0,0 +1,44 @@
+﻿namespace Ancient.ProjectSystem
+{
+    using System.Collections.Generic;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+    using NuGet.Versioning;
+
+    public class RuneSpec
+    {
+        [JsonProperty("id"), JsonRequired]
+        public string ID { get; set; }
+        [JsonProperty("version"), JsonRequired]
+        public NuGetVersion Version { get; set; }
+        [JsonProperty("type")]
+        [JsonConverter(typeof(StringEnumConverter))]
+        public PackageType Type { get; set; }
+        [JsonProperty("owner")]
+        public string Owner { get; set; }
+        [JsonProperty("files")]
+        public IList<string> Files { get; set; }
+        [JsonProperty("iconUrl")]
+        public string IconUrl { get; set; }
+        [JsonProperty("license")]
+        public string License { get; set; }
+        [JsonProperty("title")]
+        public string Title { get; set; }
+        [JsonProperty("description")]
+        public string Description { get; set; }
+        [JsonProperty("repository")]
+        public RepositoryBlob Repository { get; set; }
+
+        public class RepositoryBlob
+        {
+            public string type { get; set; }
+            public string url { get; set; }
+        }
+
+        public enum PackageType
+        {
+            Binary,
+            Source
+        }
+    }
+}
