f5782ad47094261a2b87be991dad78371abf5cb4
Yuuki Wesp <ls-micro@ya.ru>
2020-04-12 07:55:06 +0300

    import files

---
+++ csharp/Ancient.ProjectSystem/AncientLockFile.cs
@@ -0,0 +1,27 @@
+﻿namespace Ancient.ProjectSystem
+{
+    using System;
+    using System.IO;
+    using System.Reflection;
+    using Newtonsoft.Json;
+    using NuGet.Versioning;
+
+    public class AncientLockFile
+    {
+        [JsonProperty(Order = 0)]
+        public string IS_AUTO_GENERATED { get; set; } = "DON'T TOUCH THIS FILE";
+
+        public AncientLockFile() {}
+        public AncientLockFile(Assembly asm, string Registry)
+        {
+            id = Path.GetFileNameWithoutExtension(asm.GetName().Name);
+            registry = Registry;
+            version = asm.GetName().Version;
+        }
+
+        public string id { get; set; }
+        public string registry { get; set; }
+        public NuGetVersion version { get; set; }
+        public string platform { get; set; } = "any";
+    }
+}
